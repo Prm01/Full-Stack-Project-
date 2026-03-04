8ea92037af06379e24eb977c304d2e8eae7539b7
Pramod Yadav <rishabhyadav708150@gmail.com>
2026-03-04 22:44:59 +0530

    Fix case-sensitive import for Render build

--- frontend/src/pages/home.jsx
+++ frontend/src/pages/home.jsx
@@ -2,7 +2,8 @@ import React from 'react'
 import Header from '../Components/Header'
 import SpecialityMenu from '../Components/SpecialityMenu'
 import TopDoctor from '../Components/TopDoctor'
-import Banner from '../Components/banner'
+// import Banner from '../Components/banner'
+import Banner from '../Components/Banner'
 
 const home = () => {
   return (
